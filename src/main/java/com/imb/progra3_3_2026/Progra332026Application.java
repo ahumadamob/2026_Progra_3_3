7fa743de1dd76830e8749285e005e7d965d383c7
Mario Ahumada <ahumadamob@gmail.com>
2026-04-11 19:53:57 -0300

    Primer commit

---
+++ src/main/java/com/imb/progra3_3_2026/Progra332026Application.java
@@ -0,0 +1,13 @@
+package com.imb.progra3_3_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progra332026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progra332026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/imb/progra3_3_2026/Progra332026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imb.progra3_3_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progra332026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
