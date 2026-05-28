a05350af2b28338631d42418f4d0cbc25a70d2c9
Martin Rios <tincho08mb@gmail.com>
2026-05-28 20:05:21 -0300

    Entidad Servicio, hice service, repository pero falta controller

---
+++ src/main/java/com/imb/progra3_3_2026/servicio/ServicioService.java
@@ -0,0 +1,28 @@
+package com.imb.progra3_3_2026.servicio;
+
+import java.util.List;
+
+@Service
+public class ServicioService {
+	
+	public List<Servicio> getAll() {
+		
+	}
+	
+	public Servicio getById(Long id) {
+		
+	}
+	
+	public void create(Servicio servicio) {
+		
+	}
+	
+	public  void update(Servicio servicio, Long id) {
+		
+	}
+	
+	public void delete(Long id) {
+		
+	}
+
+}
