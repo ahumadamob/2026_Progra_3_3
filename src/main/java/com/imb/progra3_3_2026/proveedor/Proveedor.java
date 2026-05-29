08ccf6166ac780e310143214d9a7487ddf34e4aa
fernando sombra <fersombra10@gmail.com>
2026-05-28 22:09:32 -0300

    correccion de error en atributo id

--- src/main/java/com/imb/progra3_3_2026/proveedor/Proveedor.java
+++ src/main/java/com/imb/progra3_3_2026/proveedor/Proveedor.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Proveedor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String razonSocial;
 	private String cuil;
 	private String telefono;
@@ -21,11 +21,11 @@ public class Proveedor {
 		super();
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
