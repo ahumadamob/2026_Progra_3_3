ffb129d0780d6745ebb2234b30f16a5b75e23e62
Mario Ahumada <ahumadamob@gmail.com>
2026-06-25 22:32:59 -0300

    Consulta relacionado con Veterinaria y Mascota con ResponseEntity

--- src/main/java/com/imb/progra3_3_2026/consulta/ConsultaController.java
+++ src/main/java/com/imb/progra3_3_2026/consulta/ConsultaController.java
@@ -3,6 +3,7 @@ package com.imb.progra3_3_2026.consulta;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,31 +21,69 @@ public class ConsultaController {
 	
 	// Recuperar todas
 	@GetMapping("/consultas")
-	public List<Consulta> buscarConsulta(){
-		return service.getAll();
+	public ResponseEntity<List<Consulta>>  buscarConsulta(){
+		
+		List<Consulta> listaConsulta = service.getAll();
+		
+		if (listaConsulta.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}else {
+			return ResponseEntity.ok(listaConsulta);
+		}	
+		
 	}
 	
 	// Recuperar una sola
 	@GetMapping("/consultas/{id}")
-	public Consulta buscarConsultaPorId(@PathVariable Long id) {
-		return service.getById(id);
+	public ResponseEntity<Consulta>  buscarConsultaPorId(@PathVariable Long id) {
+		
+		Consulta consulta = service.getById(id);
+		if(consulta == null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			return ResponseEntity.ok(consulta);
+		}
 	}
 	
 	// Crear nueva consulta
 	@PostMapping("/consultas")
-	public Consulta crearNuevaConsulta(@RequestBody Consulta consulta ) {
-		return service.create(consulta);
+	public ResponseEntity<Consulta> crearNuevaConsulta(@RequestBody Consulta consulta ) {
+		try {
+			Consulta consultaCreda = service.create(consulta);
+			return ResponseEntity.ok(consultaCreda);
+		}catch(Exception e) {
+			return ResponseEntity.badRequest().build();
+		}		
 	}
 	
 	// Actualizar consulta
 	@PutMapping("/consultas/{id}")
-	public Consulta actualizarConsulta(@PathVariable Long id, @RequestBody Consulta consulta ) {
-		return service.update(consulta, id);
+	public ResponseEntity<Consulta> actualizarConsulta(@PathVariable Long id, @RequestBody Consulta consulta ) {
+		
+		Consulta consultaDesdeServicio = service.getById(id);
+		if(consultaDesdeServicio == null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			try {
+				Consulta consultaActualizada= service.update(consulta, id);
+				return ResponseEntity.ok(consultaActualizada);
+			}catch(Exception e) {
+				return ResponseEntity.badRequest().build();
+			}
+		}	
 	}	
 	
 	// Eliminar consulta
 	@DeleteMapping("/consultas/{id}")
-	public void borrarConsultaPorId(@PathVariable Long id) {
-		service.delete(id);
+	public ResponseEntity<?> borrarConsultaPorId(@PathVariable Long id) {
+		
+		Consulta consulta = service.getById(id);
+		if(consulta == null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			service.delete(id);
+			return ResponseEntity.ok().build();
+		}
+		
 	}
 }
