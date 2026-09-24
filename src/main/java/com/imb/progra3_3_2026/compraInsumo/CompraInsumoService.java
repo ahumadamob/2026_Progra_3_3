8db5df48157371c8f49cb2721ee5227287d14f04
fedeal1982 <fede.tomba.27@gmail.com>
2026-09-24 14:47:05 -0300

    correcciones en CompraInsumoService

--- src/main/java/com/imb/progra3_3_2026/compraInsumo/CompraInsumoService.java
+++ src/main/java/com/imb/progra3_3_2026/compraInsumo/CompraInsumoService.java
@@ -5,34 +5,68 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.imb.progra3_3_2026.exceptions.ResourceNotFoundException;
+import com.imb.progra3_3_2026.insumoClinico.InsumoClinico;
+import com.imb.progra3_3_2026.insumoClinico.InsumoClinicoRepository;
+import com.imb.progra3_3_2026.proveedor.Proveedor;
+import com.imb.progra3_3_2026.proveedor.ProveedorRepository;
+
 @Service
 public class CompraInsumoService {
 
-    @Autowired
-    private CompraInsumoRepository repo;
+	@Autowired
+	private CompraInsumoRepository repo;
 
-    public List<CompraInsumo> getAll() {
-        return repo.findAll();
-    }
+	@Autowired
+	private ProveedorRepository proveedorRepository;
 
-    public CompraInsumo getById(Long id) {
-        return repo.findById(id).orElse(null);
-    }
+	@Autowired
+	private InsumoClinicoRepository insumoClinicoRepository;
 
-    public CompraInsumo create(CompraInsumo compraInsumo) {
-        return repo.save(compraInsumo);
-    }
+	public List<CompraInsumo> getAll() {
+		return repo.findAll();
+	}
 
-    public CompraInsumo update(Long id, CompraInsumo compraInsumo) {
-        CompraInsumo existente = this.getById(id);
-        if (existente == null) {
-            return null;
-        }
-        compraInsumo.setId(id);
-        return repo.save(compraInsumo);
-    }
+	public CompraInsumo getById(Long id) {
+		return repo.findById(id).orElse(null);
+	}
 
-    public void delete(Long id) {
-        repo.deleteById(id);
-    }
+	public CompraInsumo create(CompraInsumo compraInsumo) {
+		Proveedor proveedor = proveedorRepository.findById(compraInsumo.getProveedor().getId())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Proveedor no encontrado con id: " + compraInsumo.getProveedor().getId()));
+
+		InsumoClinico insumoClinico = insumoClinicoRepository.findById(compraInsumo.getInsumoClinico().getId())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"InsumoClinico no encontrado con id: " + compraInsumo.getInsumoClinico().getId()));
+
+		compraInsumo.setProveedor(proveedor);
+		compraInsumo.setInsumoClinico(insumoClinico);
+		return repo.save(compraInsumo);
+	}
+
+	public CompraInsumo update(CompraInsumo compraInsumo, Long id) {
+		CompraInsumo actualizarCompraInsumo = this.getById(id);
+		if (actualizarCompraInsumo == null) {
+			return null;
+		} else {
+			Proveedor proveedor = proveedorRepository.findById(compraInsumo.getProveedor().getId())
+					.orElseThrow(() -> new ResourceNotFoundException(
+							"Proveedor no encontrado con id: " + compraInsumo.getProveedor().getId()));
+
+			InsumoClinico insumoClinico = insumoClinicoRepository.findById(compraInsumo.getInsumoClinico().getId())
+					.orElseThrow(() -> new ResourceNotFoundException(
+							"InsumoClinico no encontrado con id: " + compraInsumo.getInsumoClinico().getId()));
+
+			compraInsumo.setId(id);
+			compraInsumo.setProveedor(proveedor);
+			compraInsumo.setInsumoClinico(insumoClinico);
+			return repo.save(compraInsumo);
+		}
+	}
+
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+
 }
